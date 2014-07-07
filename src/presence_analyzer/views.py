d4aa56bd4476a776ae1dc3a8a085a0f86082c535
matlobocki <mateusz.lobocki@stxnext.pl>
2014-07-07 15:53:12 +0200

    templates update

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -4,7 +4,7 @@ Defines views.
 """
 
 import calendar
-from flask import redirect
+from flask import redirect, render_template, url_for
 
 from presence_analyzer.main import app
 from presence_analyzer.utils import (
@@ -16,6 +16,7 @@ from presence_analyzer.utils import (
 )
 
 import logging
+import jinja2
 log = logging.getLogger(__name__)  # pylint: disable-msg=C0103
 
 
@@ -24,7 +25,15 @@ def mainpage():
     """
     Redirects to front page.
     """
-    return redirect('/static/presence_weekday.html')
+    return redirect(url_for('general_view', template='presence_weekday.html'))
+    #return render_template('presence_weekday.html', active_page='mainpage') 
+
+
+@app.route('/<template>')
+def general_view(template):
+    """
+    """
+    return render_template(template, active_page=template)
 
 
 @app.route('/api/v1/users', methods=['GET'])
@@ -38,6 +47,7 @@ def users_view():
             for i in data.keys()]
 
 
+@app.route('/api/v1/mean_time_weekday/', methods=['GET'])
 @app.route('/api/v1/mean_time_weekday/<int:user_id>', methods=['GET'])
 @jsonify
 def mean_time_weekday_view(user_id):
@@ -56,6 +66,7 @@ def mean_time_weekday_view(user_id):
     return result
 
 
+@app.route('/api/v1/presence_weekday/', methods=['GET'])
 @app.route('/api/v1/presence_weekday/<int:user_id>', methods=['GET'])
 @jsonify
 def presence_weekday_view(user_id):
@@ -75,6 +86,7 @@ def presence_weekday_view(user_id):
     return result
 
 
+@app.route('/api/v1/presence_start_end_view/', methods=['GET'])
 @app.route('/api/v1/presence_start_end_view/<int:user_id>', methods=['GET'])
 @jsonify
 def presence_start_end_view(user_id):
